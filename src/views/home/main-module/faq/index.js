51b9e1261cd4fb02c33206632b790a6e7605e98d
xasier <1029927373@qq.com>
2022-02-18 00:23:25 +0800

    feat : 1.faq

--- src/views/home/main-module/faq/index.js
+++ src/views/home/main-module/faq/index.js
@@ -1,7 +1,26 @@
-export default function Faq() {
-    return(
-        <div className="faq-module-container">
+import "./index.css"
+import {useState} from "react";
+import faqText    from "./faqText";
 
+export default function Faq() {
+    const [text] = useState(faqText);
+    return (
+        <div className="faq-module-container">
+            <h2>FAQ</h2>
+            <div className="faq-module-accordion">
+                <div className="faq-module-sub-container">
+                    {
+                        text.map((item, index) => (
+                            <div className="faq-module-block" key={index}>
+                                <label>{item.title}</label>
+                                <div>
+                                    {item.content}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
--- src/views/home/main-module/roadmap/index.js
+++ src/views/home/main-module/roadmap/index.js
@@ -1,11 +1,13 @@
 import "./index.css";
-import roadmapPng from "../../../../assets/images/roadmap.png";
-import unicorn    from "../../../../assets/images/unicornspesh.png";
-import sweater    from "../../../../assets/images/sweaterspesh.png";
+import roadmapPng  from "../../../../assets/images/roadmap.png";
+import roadmap2Png from "../../../../assets/images/roadmap2.png";
+import unicorn     from "../../../../assets/images/unicornspesh.png";
+import sweater     from "../../../../assets/images/sweaterspesh.png";
 
 export default function Roadmap() {
     return (
         <div className="roadmap-module-container">
+            <img className="roadmap-module-roadmap-back" src={roadmap2Png} alt="" />
             <img className="roadmap-module-roadmap" src={roadmapPng} alt="" />
             <img className="roadmap-module-left" src={unicorn} alt="" />
             <img className="roadmap-module-right" src={sweater} alt="" />
