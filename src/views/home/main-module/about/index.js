7cd107b9f2a3b2ae5073782acd9f58d34b57b963
xasier <1029927373@qq.com>
2022-02-17 23:29:38 +0800

    style : 1.字体

--- src/views/home/main-module/about/index.js
+++ src/views/home/main-module/about/index.js
@@ -7,7 +7,7 @@ export default function About() {
             <div className="about-module-main-about">
                 <div className="about-module-main-story">
                     <p className="about-module-header">SPESH'S STORY</p>
-                    <p className="about-module--main-about-subtext--2PeMC">Spesh is looking for his best friend
+                    <p className="about-module--main-about-subtext">Spesh is looking for his best friend
                         throughout Coolman's Universe. To travel through this universe, Spesh uses a surfboard and a
                         compass. This compass is no ordinary compass, however. Not only does it point Spesh in the right
                         direction to find his mysterious friend, but it also tracks the path that Spesh's friend has
