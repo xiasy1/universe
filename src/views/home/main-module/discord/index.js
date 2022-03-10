04b762589f13a4943846b5e6fcee444d0eb75585
xasier <1029927373@qq.com>
2022-03-11 00:08:30 +0800

    style : 1.add font; 2.discord-text

--- src/views/home/main-module/discord/index.js
+++ src/views/home/main-module/discord/index.js
@@ -14,21 +14,21 @@ const icons = imgArr.map(item => getAWSCDN("discord-icons", item));
 export default function Discord() {
     return (
         <div className="discord-module">
-            <img id="discord" src={getAWSCDN("discord-icons", 'background','webp')} alt="" />
+            <img id="discord" src={getAWSCDN("discord-icons", 'background', 'webp')} alt="" />
             <section className="discord-text">
                 Me-WOW! Didn't expect the world of the Catddle-verse to be discovered by you! Unusual cats are hidden
-                here. These lovely cats cuddle what they love and often hide in their litter boxes. You will never know
-                what kind of personality it has until you open.
+                here. These lovely cats cuddle what they love and often hide in their litter boxes. <br/>
+                You will never know what kind of personality it has until you open.<br/>
                 Some of them come from corners of cities, some are born from myths, some with special secrets, and some
                 can even mutate into monsters... Each of them is unique and has their own story. Would you like to adopt
                 one? Come try your luck!
             </section>
             <div className="discord-icons">
-                {/*<div className="left-icons">*/}
+                <div className="left-icons">
                     {
                         imgArr.map((item, i) => <img key={i} className={item} src={icons[i]} alt="" />)
                     }
-                {/*</div>*/}
+                </div>
             </div>
         </div>
     )
