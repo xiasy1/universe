5a49a4d1f5ae217c68ddff191ac0f41633c69c05
xasier <1029927373@qq.com>
2022-03-19 18:47:45 +0800

    style : navbar mobile

--- src/views/home/navbar/index.js
+++ src/views/home/navbar/index.js
@@ -1,22 +1,29 @@
 import "./index.css";
-import getAWSCDN             from "../../../assets/utils/getAWSCDN";
+import getAWSCDN  from "../../../assets/utils/getAWSCDN";
+import {useState} from "react";
 // import getUniqueKey          from "../../../assets/utils/getUniqueKey";
 // import {useEffect, useState} from "react";
 
 export default function Navbar() {
-    const scrollToAnchor = (anchorName) => {
-        if (anchorName) {
-            let anchorElement = document.getElementById(anchorName);
-            if (anchorElement) {
-                anchorElement.scrollIntoView({
-                    behavior: "smooth"
-                });
-            }
-        }
-    };
+    // const scrollToAnchor = (anchorName) => {
+    //     if (anchorName) {
+    //         let anchorElement = document.getElementById(anchorName);
+    //         if (anchorElement) {
+    //             anchorElement.scrollIntoView({
+    //                 behavior: "smooth"
+    //             });
+    //         }
+    //     }
+    // };
     const boatImage = getAWSCDN('navbar','boat','png');
     const twitterImage = getAWSCDN('navbar','twitter','png');
     const connectImage = getAWSCDN('navbar','connect ','png');
+    const menuImage = getAWSCDN('navbar','menu','png');
+    const [isMenuShow, showSideMenu] = useState(false)
+    const openSideMenu = e => {
+        e.preventDefault();
+        showSideMenu(true);
+    }
     return (
         <div className="navbar-nav">
             <div className="navbar-nav-items">
@@ -24,21 +31,25 @@ export default function Navbar() {
                     <a href="/">CATTDLE VERSE</a>
                 </section>
                 <section className="nav-right">
-                    <a href="/">
+                    <a className="nav-boat" href="/">
                         <img src={boatImage} alt="" />
                     </a>
-                    <a href="https://twitter.com/catddlenft" title="twitter">
+                    <a className="nav-twitter" title="twitter"
+                       href="https://twitter.com/catddlenft">
                         <img src={twitterImage} alt="" />
                     </a>
-                    <a href="/">
+                    <a className="nav-connect" href="/">
                         <img src={connectImage} alt="" />
                     </a>
+                    <a className="nav-menu" onClick={openSideMenu}>
+                        <img src={menuImage} alt="" />
+                    </a>
                 </section>
-                {/*<span onClick={() => scrollToAnchor("discord")}>DISCORD</span>*/}
-                {/*<a href="https://twitter.com/catddlenft?s=21">TWITTER</a>*/}
-                {/*<span onClick={() => scrollToAnchor("opensea-module")}>OPENSEA</span>*/}
-                {/*<span onClick={() => scrollToAnchor("looks-rare")}>LOOKSRARE</span>*/}
-                {/*<span onClick={() => scrollToAnchor("medium-module")}>MEDIUM</span>*/}
+                {/*<section className={`nav-side-menu ${isMenuShow ? 'side-menu-active' :''}`}>*/}
+                {/*    <h3 onClick={()=> showSideMenu(false)}>X</h3>*/}
+                {/*    <h3>ROADMAP</h3>*/}
+                {/*    <h3>F&Q</h3>*/}
+                {/*</section>*/}
             </div>
             <img className="logo active" src={getAWSCDN('navbar', `logo`, 'jpg')} alt="" />
         </div>
