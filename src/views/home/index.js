79e08dfd7985650df1b97b4c3fbdbe4ce56c0f98
v_xiasyxia <v_xiasyxia@tencent.com>
2022-05-18 19:51:15 +0800

    mp4 test

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,20 @@
 import Navbar     from "./navbar";
 import MainModule from "./main-module";
+import testVideo from "./test.mp4"
 
 export default function Home() {
     return (
         <div className="home">
             <Navbar />
             <MainModule />
+            <video width="300px" height="300px" src={testVideo}
+                   playsInline={true}
+                   webkit-playsinline={''}
+                   x5-playsinline={''}
+                   x-webkit-airplay={''}
+                   autoPlay={true}
+                   loop={true}
+                   muted={true} ></video>
         </div>
     );
 }
